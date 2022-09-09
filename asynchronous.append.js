0d436ca3f3b0dd267efa498bfe18375d8af874be
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2022-09-09 10:12:46 +0800

    updates

---
+++ asynchronous.append.js
@@ -0,0 +1,8 @@
+const { appendFile } = require('fs');
+
+const fileName = './data/app.log';
+const data = `163.3.217.18 - - [21/09/2019:10:07:21 -0500] "GET /write-file-test" 405 21512`;
+
+appendFile(fileName, data, (err) => {
+    err ? console.error(err) : console.log("file saved");
+});
--- asynchronous.write.js
+++ asynchronous.write.js
@@ -1,8 +1,15 @@
-const { writeFile } = require('fs');
+const { constants, writeFile } = require('fs');
 
 const fileName = './data/app.log';
+const fileName2 = './data/newapp.log';
 const data = `163.3.217.18 - - [21/09/2019:10:07:21 -0500] "GET /write-file-test" 405 21512`;
 
-writeFile(fileName, data, (err) => {
-    err ? console.error(err) : console.log("file saved");
+
+//replaces all contents
+// writeFile(fileName, data, (err) => {
+//     err ? console.error(err) : console.log("file saved");
+// });
+writeFile(fileName2, data, { encoding: 'utf8' }, 
+    (err) => {
+        err ? console.error(err) : console.log("file saved");
 });
