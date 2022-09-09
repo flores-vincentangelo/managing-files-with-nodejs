ada6e965b2edac12c2977108c493b641ad1f4f3f
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2022-09-09 10:46:35 +0800

    updates

---
+++ create.index.js
@@ -0,0 +1,15 @@
+const { closeSync, openSync, readdirSync, writeSync } = require('fs');
+const camelcase = require('camelCase')
+
+const indexFd = openSync('./index.js', 'w');
+
+const files = [];
+
+files.map(f => {
+    const name = f.replace("js", "");
+    console.log(`Adding a file: ${f}`);
+    writeSync(indexFd,
+        `module.exports.${camelcase(name)} = require('./read/${name}').read;\n`);
+});
+
+closeSync(indexFd);
